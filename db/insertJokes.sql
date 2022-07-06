b7e1471a90d9ceeefd444ae82f221afaf26d37cb
pieter-c-kok <98531340+pieter-c-kok@users.noreply.github.com>
2022-07-06 21:34:26 +0400

    Create insertJokes.sql
    Add jokes to DB.

---
+++ db/insertJokes.sql
@@ -0,0 +1,4 @@
+INSERT INTO Joke (content, inappropriate) VALUES
+  ('Why do Java programmers wear glasses? Because they can\'t see sharp.', 0),
+  ('Why did the chicken cross the road? To get to the other side.', 0),
+  ('A dyslexic man walks into a bra...', 0);

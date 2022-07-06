5136e26e57f65bd183a099e86c6f002f8f33d740
Wessel Scholtz <wessels@bbd.co.za>
2022-07-06 18:35:10 +0200

    create script to mariadb format

--- db/createTables.sql
+++ db/createTables.sql
@@ -1,16 +1,16 @@
-CREATE TABLE [Joke] (
-  [jokeId] int PRIMARY KEY IDENTITY(1, 1),
-  [content] varchar(280),
-  [inappropriate] bit
-)
-GO
+CREATE TABLE Joke (
+	jokeId INT UNSIGNED auto_increment NOT NULL,
+	content varchar(280) NOT NULL,
+	inappropriate BIT NOT NULL,
+	CONSTRAINT Joke_PK PRIMARY KEY (jokeId)
+);
 
-CREATE TABLE [JokeOfTheDay] (
-  [jokeOfTheDayId] int PRIMARY KEY IDENTITY(1, 1),
-  [day] dateTime,
-  [jokeId] int
-)
-GO
+CREATE TABLE JokeOfTheDay (
+  jokeOfTheDayId INT UNSIGNED auto_increment NOT NULL,
+  dayDate dateTime NOT NULL,
+  jokeId INT UNSIGNED NOT NULL,
+  CONSTRAINT JokeOfTheDay_PK PRIMARY KEY (jokeOfTheDayId)
+);
 
-ALTER TABLE [JokeOfTheDay] ADD FOREIGN KEY ([JokeId]) REFERENCES [Joke] ([JokeId])
-GO
+ALTER TABLE JokeOfTheDay ADD FOREIGN KEY (JokeId) REFERENCES Joke (JokeId);
+

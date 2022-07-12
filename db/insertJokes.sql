af5aa14e406d7e7e1328a91a0db7ec4e8893dd50
Wessel Scholtz <wessels@bbd.co.za>
2022-07-13 01:54:20 +0200

    Added a few days

--- db/insertJokes.sql
+++ db/insertJokes.sql
@@ -3,3 +3,9 @@ INSERT INTO Joke (content, inappropriate) VALUES
   ('Why did the chicken cross the road? To get to the other side.', 0),
   ('A dyslexic man walks into a bra...', 0),
   ('What is the difference between a duck and a goat? One mucks around in a fountain and the other fucks around on a mountain.',1);
+
+INSERT INTO JokeOfTheDay (dayDate, jokeId) VALUES
+  ('2022-07-13', 1),
+  ('2022-07-14', 2),
+  ('2022-07-15', 3),
+  ('2022-07-16', 4);

32791c1dbc7417d898d9eab3093933f165172ed1
Pieter Kok <pieterk@bbd.co.za>
2022-07-12 20:45:44 +0200

    Add dashboard logout button.

--- frontend/jokesapi/src/app/services/login.service.ts
+++ frontend/jokesapi/src/app/services/login.service.ts
@@ -22,4 +22,17 @@ export class LoginService {
 
     window.location.href = "https://jokesapi.auth.eu-west-1.amazoncognito.com/oauth2/authorize?client_id=632uqqj15rj2j2u3mhm38qook2&response_type=code&scope=email+jokesapi%2Fjoke.read+jokesapi%2Fjoke.write+openid&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fsession"
   }
+
+  gotoLogout() {
+    const logoutEndpoint = "/logout"
+
+    if (environment.production) {
+      //Problably router instead
+      window.location.href = logoutEndpoint
+    } else if (environment.development) {
+      window.location.href = environment.devURL + logoutEndpoint
+    }
+
+    window.location.href = "https://jokesapi.auth.eu-west-1.amazoncognito.com/logout?client_id=632uqqj15rj2j2u3mhm38qook2&logout_uri=http://localhost:4200/loggedout";
+  }
 }

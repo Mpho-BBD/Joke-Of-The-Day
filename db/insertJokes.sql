e5288fff94e0143cb863dd29636637e249d0d23a
Conrad <conrad@bbd.co.za>
2022-07-13 02:24:37 +0200

    Added a few more jokes

--- db/insertJokes.sql
+++ db/insertJokes.sql
@@ -1,11 +1,28 @@
 INSERT INTO Joke (content, inappropriate) VALUES
-  ('Why do Java programmers wear glasses? Because they can\'t see sharp.', 0),
+  ('Why do Java programmers wear glasses? Because they can''t see sharp.', 0),
   ('Why did the chicken cross the road? To get to the other side.', 0),
   ('A dyslexic man walks into a bra...', 0),
-  ('What is the difference between a duck and a goat? One mucks around in a fountain and the other fucks around on a mountain.',1);
+  ('What is the difference between a duck and a goat? One mucks around in a fountain and the other fucks around on a mountain.',1)
+  ('Once you''ve seen one shopping center, you''ve seen the mall.', 0),
+  ('I was up all night wondering where the sun went, but then it dawned on me.', 0),
+  ('When my son told me to stop impersonating a flamingo, I had to put my foot down.', 0),
+  ('Shout out to my fingers. I can always count on them.', 0),
+  ('"Give it to me! Give it to me!" she yelled. "I''m so wet, give it to me now!" She could scream all she wanted, but I was keeping the umbrella.', 1),
+  ('What''s Thanos'' favorite app on his phone? Snapchat.', 0),
+  ('Why did the computer show up late for work? It had a hard drive.', 0),
+  ('What''s the difference between a  poorly dressed man on a unicycle and a well-dressed man on a bicycle? Attire.', 0),
+  ('I''m a big fan of whiteboards. I find them quite re-markable.', 0),
+  ('I was going to make myself a belt made out of watches, but then I realized it would be a waist of time.', 0);
 
 INSERT INTO JokeOfTheDay (dayDate, jokeId) VALUES
   ('2022-07-13', 1),
   ('2022-07-14', 2),
   ('2022-07-15', 3),
-  ('2022-07-16', 4);
+  ('2022-07-16', 5),
+  ('2022-07-17', 6),
+  ('2022-07-18', 7),
+  ('2022-07-20', 8),
+  ('2022-07-22', 10),
+  ('2022-07-23', 11),
+  ('2022-07-24', 12),
+  ('2022-07-25', 13);
